a7d685c14c42a79ad00c2a1b43c4288c412fa314
Doody Parizada <doody.parizada@gmail.com>
2019-05-26 18:11:05 +0300

    optimize for zero balance

--- src/app.py
+++ src/app.py
@@ -18,7 +18,8 @@ from .helpers import (get_proxy_address,
                       sign_tx,
                       build_migration_transaction,
                       build_create_transaction,
-                      is_burned, get_account_data)
+                      is_burned, get_kin2_account_data,
+                      get_kin3_account_data_or_none)
 
 logger = logging.getLogger('migration')
 HTTP_STATUS_OK = 200
@@ -40,6 +41,57 @@ def account_status(account_address):
     }), HTTP_STATUS_OK
 
 
+def migrate_zero_balance(account_address, kin3_account_data):
+    """migrate an account with zero balance."""
+    if kin3_account_data is None:
+        try:
+            main_account.create_account(account_address, starting_balance=0, fee=0)
+            logger.info(f'Address: {account_address}, was not pre-created, created now')
+        except KinErrors.AccountExistsError:
+            pass
+        statsd.increment('accounts_migrated', tags=['had_account:false', 'zero:true'])
+    else:
+        statsd.increment('accounts_migrated', tags=['had_account:true', 'zero:true'])
+    return flask.jsonify({'code': HTTP_STATUS_OK, 'message': 'OK', 'balance': 0}), HTTP_STATUS_OK
+
+
+def migrate_balance(account_address, proxy_address, channel, old_balance):
+    """migrate account with non zero balance."""
+    # Get tx builder, fee is 0 since we are whitelisted
+    builder = main_account.get_transaction_builder(0)
+    # Add the memo manually because use the builder directly
+    builder.add_text_memo(build_memo(main_account.app_id, None))
+    # Build tx
+    build_migration_transaction(builder, proxy_address, account_address, old_balance)
+    sign_tx(builder, channel, main_account.keypair.secret_seed)
+
+    try:
+        return main_account.submit_transaction(builder)
+    except KinErrors.AccountExistsError:
+        # The proxy was already created, so migration already happened
+        raise MigrationErrors.AlreadyMigratedError(account_address)
+    except KinErrors.AccountNotFoundError:
+        return None
+
+
+def migrate_balance_and_create_account(account_address, proxy_address, channel, old_balance):
+    """migrate and create account with non zero balance."""
+    # The user's account was not pre-created on the new blockchain
+    logger.info(f'Address: {account_address}, was not pre-created, creating now')
+    # Get tx builder, fee is 0 since we are whitelisted
+    builder = main_account.get_transaction_builder(0)
+
+    # Add the memo manually because use the builder directly
+    builder.add_text_memo(build_memo(main_account.app_id, None))
+    build_create_transaction(builder, proxy_address, account_address, old_balance)
+    sign_tx(builder, channel, main_account.keypair.secret_seed)
+    try:
+        return main_account.submit_transaction(builder)
+    except KinErrors.AccountExistsError:
+        # Race condition, the client sent two migration requests at once, one of them finished first
+        raise MigrationErrors.AlreadyMigratedError(account_address)
+
+
 @app.route('/migrate', methods=['POST'])
 def migrate():
     account_address = flask.request.args.get('address', '')
@@ -49,62 +101,35 @@ def migrate():
         raise MigrationErrors.AccountNotBurnedError(account_address)
     logger.info(f'Verified that account {account_address} is burned')
 
-    account_data = get_account_data(account_address)
+    kin2_account_data = get_kin2_account_data(account_address)
+    kin3_account_data = get_kin3_account_data_or_none(account_address)  # can run in parallel with above
 
     # Get the account's old balance
-    old_balance = get_old_balance(account_data, KIN_ISSUER)
+    old_balance = get_old_balance(kin2_account_data, KIN_ISSUER)
     logger.info(f'Account {account_address} had {old_balance} kin')
+
+    if old_balance == 0:
+        return migrate_zero_balance(account_address, kin3_account_data)
 
     # Generate the keypair for the proxy account
     proxy_address = get_proxy_address(account_address, PROXY_SALT)
     logger.info(f'Generated proxy account with address: {proxy_address}')
 
-    # Get tx builder, fee is 0 since we are whitelisted
-    builder = main_account.get_transaction_builder(0)
-
-    # Add the memo manually because use the builder directly
-    builder.add_text_memo(build_memo(main_account.app_id, None))
-
-    # Build tx
-    build_migration_transaction(builder, proxy_address, account_address, old_balance)
     # Grab an available channel:
     with main_account.channel_manager.get_channel() as channel:
-        sign_tx(builder, channel, main_account.keypair.secret_seed)
+        tx_hash = None
+        if kin3_account_data:
+            tx_hash = migrate_balance(account_address, proxy_address, channel, old_balance)
 
-        try:
-            tx_hash = main_account.submit_transaction(builder)
-        except KinErrors.AccountExistsError:
-            # The proxy was already created, so migration already happened
-            raise MigrationErrors.AlreadyMigratedError(account_address)
-        except KinErrors.AccountNotFoundError:
-            # We expect most account to be created, so its better to "ask for forgiveness, not for permission"
-            # The user's account was not pre-created on the new blockchain
-            logger.info(f'Address: {account_address}, was not pre-created, creating now')
-            # Get tx builder, fee is 0 since we are whitelisted
-            builder = main_account.get_transaction_builder(0)
-
-            # Add the memo manually because use the builder directly
-            builder.add_text_memo(build_memo(main_account.app_id, None))
-            build_create_transaction(builder, proxy_address, account_address, old_balance)
-            sign_tx(builder, channel, main_account.keypair.secret_seed)
-            try:
-                tx_hash = main_account.submit_transaction(builder)
-            except KinErrors.AccountExistsError:
-                # Race condition, the client sent two migration requests at once, one of them finished first
-                raise MigrationErrors.AlreadyMigratedError(account_address)
-
-    # If the user had 0 kin, we didn't try to pay him, and might missed that he is not created
-    if old_balance == 0:
-        try:
-            main_account.create_account(account_address, starting_balance=old_balance, fee=0)
-            logger.info(f'Address: {account_address}, was not pre-created, created now')
-        except KinErrors.AccountExistsError:
-            pass
+        if tx_hash:  # migration succeeded above
+            statsd.increment('accounts_migrated', tags=['had_account:true', 'zero:false'])
+        else:
+            tx_hash = migrate_balance_and_create_account(account_address, proxy_address, channel, old_balance)
+            statsd.increment('accounts_migrated', tags=['had_account:false', 'zero:false'])
 
     logger.info(f'Successfully migrated address: {account_address} with {old_balance} balance, tx: {tx_hash}')
-    statsd.increment('accounts_migrated')
-    if old_balance > 0:
-        statsd.increment('kin_migrated', value=old_balance)
+
+    statsd.increment('kin_migrated', value=old_balance)
 
     # calls marketplace-internal for updating wallet with created_date_kin3
     marking_as_burnt_address = f'{ INTERNAL_ADDRESS }/wallets/{ account_address }/burnt'

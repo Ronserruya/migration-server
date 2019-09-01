8249ed5a10bc5c0509ecf86ca6c816fd7d40e35c
Victor <doc999tor@gmail.com>
2019-09-01 13:24:56 +0300

    fix typo

--- src/app.py
+++ src/app.py
@@ -68,7 +68,7 @@ def migrate():
 
 
 
-    return flask.jsonify({'code': 4101, 'message': 'OK', 'balance': migrated_balance }), 410
+    return flask.jsonify({'code': 4101, 'message': 'OK', 'balance': 'migrated_balance' }), 410
 
 
 @app.route('/status', methods=['GET'])
